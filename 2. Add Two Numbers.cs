7e1b2057c3c626bb10fdab663232b0f878f03bdb
skyc2298 <skyc2298@gmail.com>
2020-03-30 22:21:21 +0800

    2. Add Two Numbers solved

---
+++ 2. Add Two Numbers.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode first = l1, second = l2, currentHead = dummyHead;
+        int carry = 0;
+        while(first!=null||second!=null) {
+            int x = (first!=null)?first.val:0;
+            int y = (second!=null)?second.val:0;
+            int sum = x+y+carry;
+            carry = sum/10;
+            currentHead.next = new ListNode(sum%10);
+            currentHead = currentHead.next;
+            first = (first!=null)?first.next:first;
+            second = (second!=null)?second.next:second;
+        }
+        if(carry>0) {
+            currentHead.next = new ListNode(carry);
+        } 
+          return dummyHead.next;
+    }
+}
