3140c533d3e7bc20bfff6a2997af6f18ac70c83e
skyc2298 <skyc2298@gmail.com>
2020-04-08 18:05:58 +0800

    21. Merge Two Sorted Lists solved

--- 21. Merge Two Sorted Lists.cs
+++ 21. Merge Two Sorted Lists.cs
@@ -8,9 +8,30 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode res = (l1.val<l2.val)?l1:l2;
-        while(l1.next!=null||l2.next!=null) {
-            
+            ListNode head = new ListNode(0);
+            ListNode current = head;
+
+            while((l1!=null) && (l2!= null)) {
+                if(l1.val<=l2.val) {
+                    current.next = l1;
+                    l1=l1.next;
+                 } 
+                   else {
+                    current.next = l2;
+                    l2=l2.next;
+                }
+                
+            current = current.next;          
         }
+        
+            
+             if(l1!=null) {
+                    current.next = l1;
+                }
+                
+              if(l2!=null) {
+                    current.next = l2;
+                }
+        return head.next;
     }
 }
