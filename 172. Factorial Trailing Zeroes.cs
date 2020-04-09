f7964bcf28b64f74bca48177c82ffdb0277a70dd
skyc2298 <skyc2298@gmail.com>
2020-04-09 19:10:30 +0800

    172. Factorial Trailing Zeroes solved

---
+++ 172. Factorial Trailing Zeroes.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int TrailingZeroes(int n) {
+        if(n<5) {return 0;}
+        int count = 0;
+        while(n>=5) {
+            count+=n/5;
+            n=n/5;
+        }
+        return count;
+    }
+}
