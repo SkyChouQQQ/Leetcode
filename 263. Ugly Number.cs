0fbb0d2e65fdaad7d881dc1c339c13e18bb10bc1
skyisdog <sky@light-year.tech>
2020-06-03 16:13:13 +0800

    263 solved

---
+++ 263. Ugly Number.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsUgly(int num) {
+        while(num>=2)
+        {
+            if(num%2==0) { num/=2;}
+            else if(num%3==0) {num/=3;}
+            else if(num%5==0) {num/=5;}
+            else { return false;}
+        }
+        
+        return num==1;
+    }
+}
